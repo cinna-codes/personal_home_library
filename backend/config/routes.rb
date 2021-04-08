2785a7c58c1446cde473d537a03351569862c1ae
cinna-codes <somethingfellipickeditup@gmail.com>
2021-04-08 17:28:26 -0400

    added nested resource: author show now has a nested resource of books index

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :authors, only: [:index, :show, :create]
+  resources :authors, only: [:index, :show, :create] do
+    resources :books, only: [:index]
+  end
   resources :books, only: [:index, :show, :create]
 end
