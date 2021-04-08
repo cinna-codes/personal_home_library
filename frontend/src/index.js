856d2d6f144d8baba0808fc9f0dac969ce9d79cc
cinna-codes <somethingfellipickeditup@gmail.com>
2021-04-08 00:12:03 -0400

    shorthand code planning in comments

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ const headerHome = document.getElementById('headerHome')
 const headerBooks = document.getElementById('headerBooks')
 const headerAuthors = document.getElementById('headerAuthors')
 
+// books index
 fetch("http://localhost:3000/books")
 .then((res) => res.json())
 .then((books) => renderBooks(books))
@@ -12,3 +13,40 @@ const renderBooks = function(books) {
         rootEl.innerHTML += `<p>${book.title}</p>`
     })
 }
+// books index
+
+// books show
+    // fetch request
+    // rootEl.innerHTML = ``
+    // rootEl.innerHTML += `<p>book title</p> <p>book author_name</p> <p>book summary</p>`
+// books show
+
+// authors index
+    // fetch request
+    // clear rootEl (has to come BEFORE iterating)
+    // iterate over authors
+        // rootEl.innerHTML += `<p>author name</p>`
+// authors index
+
+// authors show
+    // fetch request 
+    // clear rootEl
+    // rootEl.innerHTML += `<p>author name</p> <p>author bio</p>`
+    // iterate over author's books
+    // rootEl.innerHTML += `<p>book title, book pagecount</p>`
+// authors show
+
+// home 
+    // clear rootEl
+    // rootEl.innerHTML += `New Book` give id
+    // rootEl.innerHTML += `New Author` give id
+    // addEventListener to both
+// home
+
+// books new
+    // fetch post request for creating book
+// books new
+
+// authors new
+    // fetch post request for creating author
+// authors new
