eda33c515d27504dcac93ed96b3c6c11e69b41a9
cinna-codes <somethingfellipickeditup@gmail.com>
2021-04-07 23:50:51 -0400

    added basic header + assigned consts for each category

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,2 +1,14 @@
-// fetch("http://localhost:3000/books")
-// .then(res => res.json())
+const rootEl = document.getElementById('root')
+const headerHome = document.getElementById('headerHome')
+const headerBooks = document.getElementById('headerBooks')
+const headerAuthors = documet.getElementById('headerAuthors')
+
+fetch("http://localhost:3000/books")
+.then((res) => res.json())
+.then((books) => renderBooks(books))
+
+const renderBooks = function(books) {
+    books.forEach(book => {
+        rootEl.innerHTML += `<p>${book.title}</p>`
+    })
+}
